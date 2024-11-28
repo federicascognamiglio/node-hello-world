e7ca5541b72b4ab25ca8abd90b61ba31cb198a9f
federicascognamiglio <scognamigliofede99@gmail.com>
2024-11-28 17:47:18 +0100

    feat: conta-vocali completed

--- conta-vocali/conta-vocali.js
+++ conta-vocali/conta-vocali.js
@@ -2,9 +2,9 @@ const arguments = process.argv;
 const vowels = ["a", "e", "i", "o", "u"];
 
 let vowelsNum = 0;
-for (let i = 0; i < vowels.length; i++) {
-    curVowel = vowels[i];
-    if (arguments[2].includes(curVowel)) {
+for (let i = 0; i < arguments[2].length; i++) {
+    const curChar = arguments[2][i];
+    if (vowels.includes(curChar)) {
         vowelsNum += 1;
     }
 }
